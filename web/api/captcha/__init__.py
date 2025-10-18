bf4c488c47650a67629ae5be0db072017ce19a41
powergaming9999-jpg <powergaming9999@gmail.com>
2025-10-19 01:09:40 +0200

    Update __init__.py

--- web/api/captcha/__init__.py
+++ web/api/captcha/__init__.py
@@ -2,12 +2,10 @@ from flask import request
 from flask import jsonify
 
 from web.app import app, limiter
-import utils
 
 
 
 @limiter.limit('1 per second')
 @app.route('/api/v1/captcha', methods=['GET'])
 def route_captcha():
-    captcha = utils.Captcha(request.access_route[0])
-    return jsonify({'jwt': captcha.to_jwt(), 'cd': captcha.cdata, 'key': captcha.site_key}), 200
+    return jsonify({'error': True, 'message': 'Captcha is disabled'}), 410
