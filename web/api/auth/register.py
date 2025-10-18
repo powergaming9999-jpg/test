bde4d63b59c39c6d34485171d133105e576ca191
powergaming9999-jpg <powergaming9999@gmail.com>
2025-10-19 01:11:29 +0200

    Update register.py

--- web/api/auth/register.py
+++ web/api/auth/register.py
@@ -18,22 +18,10 @@ def route_register():
         request_data = {
             'email':    utils.typ(request.json['email'], str).lower(),
             'name':     utils.typ(request.json['name'], str),
-            'password': utils.typ(request.json['password'], str),
-            'captcha':  utils.typ(request.json['captcha'], str),
-            'jwt':      utils.typ(request.json['jwt'], str)
+            'password': utils.typ(request.json['password'], str)
         }
     except KeyError:
         return jsonify({'error': True, 'message': 'Bad Request.'}), 400
-    
-    # Validate JWT + Turnstile
-    try:
-        captcha = utils.Captcha.from_jwt(request_data['jwt'])
-    except utils.turnstile.CaptchaJWTInvalid:
-        return jsonify({'error': True, 'message': 'Invalid or expired JWT. Refresh the page.'}), 400
-    
-    captcha_result = captcha.check(request_data['captcha'], request.access_route[0])
-    if not captcha_result.passed:
-        return jsonify({'error': True, 'message': 'CAPTCHA was invalid, or expired. Please try again.'}), 400
     
     # Validate email, username, password, and display name
     if not utils.validate_string(request_data['name'], 'username', 3, 32):
@@ -55,7 +43,7 @@ def route_register():
             'SELECT email, name FROM users WHERE email = ? OR name = ?;',
             [request_data['email'], request_data['name']]
         )
-        raw_data = cur.fetchmany(2) # can only be a maximum of 2: either name, or email is used
+        raw_data = cur.fetchmany(2)
     except db.error as exc:
         db.pool.release(conn, cur)
         raise exc
